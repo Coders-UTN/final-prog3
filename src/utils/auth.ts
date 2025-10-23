f5b4b8f3eec809dd030621564f8b65a7ff6125d9
rodry <rodrymza@gmail.com>
2025-10-23 03:25:30 -0300

    Actualiza las funciones de registro e inicio de sesión para devolver AuthResponse y manejar el token JWT

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,5 @@
-import { type IUser } from "../types/IUser";
+import type { AuthResponse } from "../types/IAuthResponse";
+import type { ILoginReq } from "../types/ILoginReq";
 import type { IUserData } from "../types/IUserData";
 
 // La URL base de tu backend Spring Boot
@@ -8,40 +9,48 @@ const API_URL = "http://localhost:8080/api/auth";
  * Registra un nuevo usuario.
  * 'userData' es un objeto con los datos del formulario de registro.
  */
-export const register = async (userData: IUserData): Promise<IUser> => {
-    const response = await fetch(`${API_URL}/register`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(userData)
-    });
+export const register = async (userData: IUserData): Promise<AuthResponse> => {
+  const response = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
 
-    if (!response.ok) {
-        // Lanza un error para que el .catch() lo reciba
-        throw new Error(`Error en el registro: ${response.statusText}`);
-    }
+  if (!response.ok) {
+    // Lanza un error para que el .catch() lo reciba
+    throw new Error(`Error en el registro: ${response.statusText}`);
+  }
 
-    return response.json(); // Devuelve el usuario creado (IUser)
+  const data: AuthResponse = await response.json();
+  console.log("Datos de respuesta:", data);
+
+  localStorage.setItem("jwt_token", data.token);
+
+  return data;
 };
 
 /**
  * Inicia sesión.
  * 'credentials' es un objeto con { email, password }
  */
-export const login = async (credentials: any): Promise<IUser> => {
-    const response = await fetch(`${API_URL}/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    });
+export const login = async (credentials: ILoginReq): Promise<AuthResponse> => {
+  const response = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
 
-    if (!response.ok) { // Ej: 401 Unauthorized
-        throw new Error('Email o contraseña inválidos');
-    }
+  const data: AuthResponse = await response.json();
 
-    // Devuelve los datos del usuario logueado (IUser)
-    return response.json();
+  if (!response.ok) {
+    // Ej: 401 Unauthorized
+    throw new Error("Email o contraseña inválidos");
+  }
+
+  // Devuelve los datos del usuario logueado (IUser)
+  return data;
 };
