6e2ec8f4a73f98eb51534ba0dcb5cc37a6eeafad
rodry <rodrymza@gmail.com>
2025-10-31 00:30:26 -0300

    creada interfaz de categoria

---
+++ src/types/ICategoria.ts
@@ -0,0 +1,12 @@
+export interface ICategoria {
+    id : number,
+    nombre : string,
+    descripcion : string,
+    imagen : string
+}
+
+export interface ICreateCategoria {
+    nombre : string,
+    descripcion : string,
+    imagen : string
+}
